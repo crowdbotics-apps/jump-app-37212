5cd6bb659328492590faa28d48b7ebef0e8fba08
Crowdbotics <team@crowdbotics.com>
2022-10-03 21:14:29 +0000

    Initial commit.

---
+++ backend/jump_app_37212/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jump_app_37212 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jump_app_37212.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "jump_app_37212",
+  url: "https://jump_app_37212.botics.co",
+  api: "https://jump_app_37212.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
